7f4dac3171ac1e4f2b8008ea1236750d89c2dc31
dichgrem <brcefy@gmail.com>
2025-11-12 21:12:50 +0800

    log:test3
    date:2025-11-12

--- projects/02_basic_ir/rtconfig.py
+++ projects/02_basic_ir/rtconfig.py
@@ -15,9 +15,9 @@ if os.getenv('RTT_ROOT'):
 
 # cross_tool provides the cross compiler
 # EXEC_PATH is the compiler execute path, for example, CodeSourcery, Keil MDK, IAR
-if  CROSS_TOOL == 'gcc':
+if CROSS_TOOL == 'gcc':
     PLATFORM    = 'gcc'
-    EXEC_PATH   = r'C:\Users\XXYYZZ'
+    EXEC_PATH   = r'/usr/bin'
 elif CROSS_TOOL == 'keil':
     PLATFORM    = 'armcc'
     EXEC_PATH   = r'C:/Keil_v5'
